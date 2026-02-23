5af2edd0ac13d1bf2e28413a4c74c59bfa19182c
Antonio <antojoseabad@gmail.com>
2026-02-23 12:35:27 +0100

    feat: add navbar and navigation

--- src/app/country/pages/layouts/country-layout/country-layout.ts
+++ src/app/country/pages/layouts/country-layout/country-layout.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Footer } from "../../../shared/footer/footer";
+import { TopMenu } from "../../../components/top-menu/top-menu";
 
 @Component({
   selector: 'app-country-layout',
-  imports: [RouterOutlet, Footer],
+  imports: [RouterOutlet, TopMenu],
   templateUrl: './country-layout.html',
 })
 export class CountryLayout { }
