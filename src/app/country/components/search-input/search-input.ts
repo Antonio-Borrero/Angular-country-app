ae641406871ada9a071fac4ccac2d2f16ce19ebc
Antonio <antojoseabad@gmail.com>
2026-03-04 10:21:36 +0100

    feat(search): add debounce to automatic search input

--- src/app/country/components/search-input/search-input.ts
+++ src/app/country/components/search-input/search-input.ts
@@ -1,4 +1,4 @@
-import { Component, input, output } from '@angular/core';
+import { Component, effect, input, output, signal } from '@angular/core';
 
 @Component({
   selector: 'search-input',
@@ -6,6 +6,19 @@ import { Component, input, output } from '@angular/core';
   templateUrl: './search-input.html',
 })
 export class SearchInput {
-  search = output<string>()
-  placeholder = input<string>()
+  search = output<string>();
+  placeholder = input<string>();
+  inputValue = signal<string>("");
+  debounceTime = input<number>()
+
+  debounceEffect = effect((onCleanup) => {
+    const value = this.inputValue();
+    const timeOut = setTimeout(() => {
+      this.search.emit(value);
+    }, this.debounceTime());
+
+    onCleanup(() => {
+      clearTimeout(timeOut)
+    })
+  }) 
 }
