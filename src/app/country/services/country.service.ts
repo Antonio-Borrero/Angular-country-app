3b04412d4f65c0b093997cca99771f7be4641b04
Antonio <antojoseabad@gmail.com>
2026-03-04 10:41:01 +0100

    feat(api): add caching for search requests

--- src/app/country/services/country.service.ts
+++ src/app/country/services/country.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import { RESTCountriesInterface } from '../interfaces/RESTCountries.interface';
-import { catchError, map, Observable, throwError } from 'rxjs';
+import { catchError, map, Observable, of, tap, throwError } from 'rxjs';
 import { Country } from '../interfaces/Country.interface';
 import { CountryMapper } from '../mappers/country.mapper';
 
@@ -11,13 +11,18 @@ import { CountryMapper } from '../mappers/country.mapper';
 })
 export class CountryService {
 
-  private http = inject(HttpClient)
+  private http = inject(HttpClient);
+  private queryCacheCountry = new Map<string, Country[]>();
+  private queryCacheCapital = new Map<string, Country[]>();
 
-  searchByCountry(query: string) {
+  searchByCountry(query: string): Observable<Country[]> {
     query = query.toLocaleLowerCase();
+
+    if (this.queryCacheCountry.has(query)) return of(this.queryCacheCountry.get(query) ?? []);
 
     return this.http.get<RESTCountriesInterface[]>(`${environment.RESTCountriesApiKey}/name/${query}`).pipe(
       map((countries) => CountryMapper.mapRestCountriesArrayToCountryArray(countries)),
+      tap((countries) => this.queryCacheCountry.set(query, countries)),
       catchError((error) => {
         console.log(`Error fetching`, error)
         return throwError(() => new Error(`No se pudo obtener países con: ${query}`))
@@ -25,11 +30,14 @@ export class CountryService {
     )
   }
 
-  searchByCapital(query: string) {
+  searchByCapital(query: string): Observable<Country[]> {
     query = query.toLocaleLowerCase();
+
+    if (this.queryCacheCapital.has(query)) return of(this.queryCacheCapital.get(query) ?? []);
 
     return this.http.get<RESTCountriesInterface[]>(`${environment.RESTCountriesApiKey}/capital/${query}`).pipe(
       map((countries) => CountryMapper.mapRestCountriesArrayToCountryArray(countries)),
+      tap((countries) => this.queryCacheCapital.set(query, countries)),
       catchError((error) => {
         console.log(`Error fetching`, error)
         return throwError(() => new Error(`No se pudo obtener países con: ${query}`))
